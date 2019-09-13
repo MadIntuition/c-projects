906940155140fd0e2537b35c3d3c9d3b1a1e0a4c
MadIntuition <44321280+MadIntuition@users.noreply.github.com>
2019-09-13 13:48:35 +0300

    Add files via upload

---
+++ RSA/RSA/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(287, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "шифр";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(43, 87);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(319, 271);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(405, 87);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(471, 271);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(405, 364);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "дешифр";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(118, 8);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(794, 73);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "здесь будут данные по шифрованию";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "количество бит";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 409);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RSA/RSA/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        public static Random rand = new Random();
+        BigInteger[] keys;
+        public BigInteger ExtendedEvklidAlgorithm(BigInteger A, BigInteger B)       // A это p, B - для чего ищем обратный элемент
+        {
+            BigInteger prevA = A, prevB = B, AmodB = A % B;
+            List<BigInteger> arrayAdivB = new List<BigInteger>();
+            arrayAdivB.Add(A / B);
+            int i = 0;
+            for (; AmodB != 0; i++)
+            {
+                prevA = prevB;
+                prevB = AmodB;
+                AmodB = prevA % prevB;
+                arrayAdivB.Add(prevA / prevB);
+            }
+            BigInteger Xi = 0, Yi = 1, temp;
+            for (i = i - 1; i >= 0; i--)
+            {
+                temp = Xi;
+                Xi = Yi;
+                Yi = temp - Yi * (arrayAdivB[i]);
+            }
+            if (Yi < 0) Yi += A;
+            return Yi;
+        }
+
+        public bool MillerRabinTest(BigInteger n, int c)
+        {
+            if (n % 2 == 0)
+                return false;
+            BigInteger step = n - 1, s = 0, d;
+            while (step % 2 == 0)
+            {
+                step /= 2;
+                s++;
+            }
+            d = step;
+            // MessageBox.Show(s.ToString()+" "+d.ToString());
+            BigInteger[] k = new BigInteger[c];
+            for (int i = 0; i < c; i++)
+            {
+                int x = rand.Next(2, c + 3);
+                while (k.Contains(x))
+                {
+                    x = rand.Next(2, c + 3);
+                }
+                k[i] = x;
+                bool flajok = false;
+                if (BigInteger.ModPow(x, d, n) == 1 || BigInteger.ModPow(x, d, n) % n == n - 1) return true;
+                else
+                {
+                    for (int r = 1; r < s; r++)
+                    {
+                        if (BigInteger.ModPow(x, (int)Math.Pow(2, r) * d, n) % n == n - 1)
+                        {
+                            flajok = true;
+                            break;
+                        }
+                    }
+                }
+                if (flajok == false) return false;
+            }
+            return true;
+        }
+        
+        public BigInteger Generation(int p)
+        {
+            //int R = rand.Next(p, 4 * p + 3);
+            //BigInteger n = (BigInteger)p * (BigInteger)R + 1;
+            BigInteger R;
+            p /= 8;
+            do
+            {
+                //R = rand.Next(p, 4 * p + 3);
+                byte[] bytes = new byte[p];
+                rand.NextBytes(bytes);
+                bytes[bytes.Length - 1] &= (byte)0x7F;
+                R = new BigInteger(bytes);
+                //n = (BigInteger)p * (BigInteger)R + 1;
+            } while (!MillerRabinTest(R, 20));
+
+            return R;
+        }
+
+        public static BigInteger RandomIntegerBelow(BigInteger N)
+        {
+            byte[] bytes = N.ToByteArray();
+            BigInteger R;
+            do
+            {
+                rand.NextBytes(bytes);
+                bytes[bytes.Length - 1] &= (byte)0x7F; 
+                R = new BigInteger(bytes);
+            } while (R >= N||R<2);
+
+            return R;
+        }
+
+        public BigInteger[] GenerationKeys(int basis)
+        {
+            BigInteger p, q, e;
+            do
+            {
+                p = Generation(basis);
+                q = Generation(basis);
+            } while (p == q);
+            BigInteger n = p * q;//открый ключ
+            BigInteger EilerFunc = (p - 1) * (q - 1);
+            do
+            {
+                e = RandomIntegerBelow(n); //открый ключ
+            } while (BigInteger.GreatestCommonDivisor(EilerFunc, e) != 1) ;
+
+            BigInteger d = ExtendedEvklidAlgorithm(EilerFunc, e); //закрытыйключ
+            BigInteger[] result = { e, n, d};
+            richTextBox3.Text = "p=" + p.ToString() + "\nq=" + q.ToString() + "\nn=" + n.ToString() + "\nEilerFunc=" + EilerFunc.ToString() + "\ne=" + e.ToString() + "\nd=" + d.ToString();
+            return result; 
+        }
+        public string RSA(string text, BigInteger e, BigInteger n)
+        {
+            byte[] utf8 = Encoding.UTF8.GetBytes(text);
+            BigInteger[] shifr = new BigInteger[utf8.Length];
+            StringBuilder ciptext = new StringBuilder("");
+            for (int i = 0; i < utf8.Length; i++)
+            {
+                shifr[i] = BigInteger.ModPow((int)utf8[i], e , n);
+                //utf8[i] = (byte)((utf8[i] + randombytes[i]) % 256);
+                ciptext.Append(shifr[i]);
+                ciptext.Append(" ");
+            }
+            return ciptext.ToString();
+        }
+
+        public string encryptRSA (string text, BigInteger d, BigInteger n)
+        {
+            StringBuilder encrtext = new StringBuilder("");
+            string[] strArr = null;
+            char[] splitchar = {' '};
+            strArr = text.Split(splitchar);
+            byte[] str = new byte[strArr.Length];
+
+            for (int i = 0; i < strArr.Length-1 ; i++)
+            {
+                BigInteger code = BigInteger.ModPow(BigInteger.Parse(strArr[i]), d, n);
+                str[i] = (byte)(code);
+
+            }
+            encrtext.Append(Encoding.UTF8.GetString(str));
+            return encrtext.ToString();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            keys = GenerationKeys(int.Parse(textBox1.Text));
+            richTextBox2.Text = RSA(richTextBox1.Text, keys[0], keys[1]);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = encryptRSA(richTextBox1.Text, keys[2], keys[1]);
+        }
+    }
+}
